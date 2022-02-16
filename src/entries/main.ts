3e779f4f0bdf28fb458c1349ad9217ea60392f1e
Paul Tuck <pau1tuck@outlook.com>
2022-02-17 00:10:26 +0800

    "2022-02-17_00:10_CST"

--- src/entries/main.ts
+++ src/entries/main.ts
@@ -1,5 +1,5 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
+// import "core-js/stable";
+// import "regenerator-runtime/runtime";
 import Page from "../scripts/page";
 
 H5P = H5P || {};
