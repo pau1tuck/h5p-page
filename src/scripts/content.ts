1b2368e9f5186ec650f97604a31c8bde5cda367f
Paul Tuck <pau1tuck@outlook.com>
2022-02-16 17:46:17 +0800

    "2022-02-16_17:46_CST"

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,1 +1,57 @@
-export default class PageContent extends H5P.EventDispatcher {}
+export default class PageContent extends H5P.EventDispatcher {
+  private parent: any;
+  private behaviour: any;
+  private params: any;
+  private columnNodes: any;
+  private shouldAutoplay: any;
+  private l10n: any;
+  /**
+   * @constructor
+   *
+   * @param {object} config
+   * @param {string} contentId
+   * @param {object} contentData
+   * @param {object} parent
+   * @param {object} params
+   */
+  constructor(
+    config: any,
+    contentId: string,
+    contentData: any = {},
+    parent: any,
+    params: any
+  ) {
+    super();
+    this.parent = parent;
+    this.behaviour = config.behaviour;
+
+    this.params = params;
+
+    this.columnNodes = [];
+    this.shouldAutoplay = [];
+    this.l10n = config.l10n;
+  }
+  /**
+   * Create page content
+   *
+   * @return {HTMLElement} Page content
+   */
+  public createPageContent = (): HTMLElement => {
+    const content = document.createElement("div");
+    content.classList.add("h5p-page-content");
+    this.columnNodes.forEach((element: HTMLElement) => {
+      content.appendChild(element);
+    });
+
+    return content;
+  };
+
+  /**
+   * Create Column instances
+   *
+   * @param {object} config Parameters
+   * @param {number} contentId Content id
+   * @param {object} contentData Content data
+   */
+  private createColumns(config, contentId, contentData) {}
+}
