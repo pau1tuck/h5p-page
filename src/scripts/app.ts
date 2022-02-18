a5f24eb8101237a0972c199ab4c0d4a3d445a0be
Paul Tuck <pau1tuck@outlook.com>
2022-02-18 21:33:45 +0800

    "2022-02-18_21:33_CST"

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,75 +1,27 @@
-interface IApp {
-  config: any;
-  contentId: number;
-  contentData: any;
-}
-
-class Page extends H5P.EventDispatcher {
+export default class HelloWorld extends H5P.EventDispatcher {
   /**
-   * @constructor for H5P.Page
+   * @constructor
    *
-   * @param {object} config
-   * @param {string} contentId
-   * @param {object} contentData
+   * @param {object} params Parameters passed by the editor.
+   * @param {number} contentId Content's id.
+   * @param {object} [extras] Saved state, metadata, etc.
    */
-  constructor(params: any, contentId: number, contentData: object = {}) {
+  constructor(params: any, contentId: number, extras = {}) {
     super();
-    this.jQuery = H5P.jQuery;
-    this.contentId = contentId;
-    this.mainWrapper = null;
-
-    // Hello, World constructor
     const username: string =
       (H5PIntegration && H5PIntegration.user && H5PIntegration.user.name) ||
-      "World";
-    this.title = document.createElement("div");
-    this.title.innerText = params.titleField.replace("%username", username);
-    /**
-     * Attach library to wrapper
-     * @param {jQuery} $wrapper
-     */
-    this.attach = ($container: any) => {
-      this.mainWrapper = $container;
-      // Needed to enable scrolling in fullscreen
-      // $wrapper.addClass("h5p-page");
-
-      if (this.isEdge18orEarlier()) {
-        this.$container.addClass("edge-18");
-      }
-
-      this.$container.append(
-        `<div class="title-text">${this.params.titleField}</div>`,
-      );
-      this.$container.append(
-        `<div class="title-text">${this.params.image}</div>`,
-      );
-      this.$container.append(
-        `<div class="title-text">${this.params.greeting}</div>`,
-      );
-
-      this.$container.append(`<p>${this.title}</div>`);
-
-      /*
-      $wrapper.append(this.content.container);
-      // this.$wrapper = $wrapper; */
-    };
+      "world";
+    this.element = document.createElement("div");
+    this.element.innerText = params.textField.replace("%username", username);
 
     /**
-     * Checks if browser is IE Edge version 18 or earlier
+     * Attach library to wrapper.
+     *
+     * @param {jQuery} $wrapper Content's container.
      */
-    this.isEdge18orEarlier = () => {
-      const ua = window.navigator.userAgent;
-      const edgeIndex = ua.indexOf("Edge/");
-      if (edgeIndex < 0) {
-        return false;
-      }
-      const edgeVersion = ua.substring(
-        edgeIndex + 5,
-        ua.indexOf(".", edgeIndex),
-      );
-      return parseInt(edgeVersion, 10) <= 18;
+    this.attach = ($wrapper: any) => {
+      $wrapper.get(0).classList.add("h5p-hello-world");
+      $wrapper.get(0).appendChild(this.element);
     };
   }
 }
-
-export default Page;
